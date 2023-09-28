5528fe90d9f0a0d96f62f10d3f17dd8921f3f435
Aseem <Aseempratap2207@gmail.com>
2023-09-29 01:41:00 +0530

    initialise repo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import FirstComponent from "./FirstComponent";
+function App() {
+  return (
+    <div>
+      <FirstComponent />
+      hello
+    </div>
+  );
+}
+export default App;
---
+++ src/FirstComponent.jsx
@@ -0,0 +1,4 @@
+function FirstComponent(){
+    return <h1>Hello World</h1>;
+}
+export default FirstComponent;
