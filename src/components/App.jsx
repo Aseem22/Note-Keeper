d24c743febbb8348787abee98c6e98074ec24980
Aseem <Aseempratap2207@gmail.com>
2023-10-01 21:51:39 +0530

    add Notes component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./Footer";
+import Notes from "./Notes";
 function App() {
   return (
     <div>
       <Header />
+      <Notes />
       <Footer />
     </div>
   );
---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="Notes">
+      <h1>Notes Title</h1>
+      <p>Notes Description</p>
+    </div>
+  );
+}
+export default Notes;
